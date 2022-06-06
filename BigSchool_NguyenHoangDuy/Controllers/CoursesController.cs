4ef6d18281cb879ca41e04537521dd4d4680fefd
Nguyễn Hoàng Duy <duykibo22@gmail.com>
2022-06-06 11:11:10 +0700

    Nop bai tuan 2

--- BigSchool_NguyenHoangDuy/Controllers/CoursesController.cs
+++ BigSchool_NguyenHoangDuy/Controllers/CoursesController.cs
@@ -29,8 +29,14 @@ namespace BigSchool_NguyenHoangDuy.Controllers
         }
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(CourseViewModel viewModel)
         {
+            if (ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
             var course = new Course
             {
                 LecturerId = User.Identity.GetUserId(),
--- BigSchool_NguyenHoangDuy/Controllers/HomeController.cs
+++ BigSchool_NguyenHoangDuy/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using BigSchool_NguyenHoangDuy.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Linq;
 
 namespace BigSchool_NguyenHoangDuy.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbcontext;
+        public HomeController()
+        {
+            _dbcontext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Course> upcommingCourses = _dbcontext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.category)  
+                .Where(c => c.DateTime > DateTime.Now).ToList();
+            return View(upcommingCourses);
         }
 
         public ActionResult About()
